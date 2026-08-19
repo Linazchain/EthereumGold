75edc04dd398a965caf269ded242d9f97e5b62e0
Linazchain <gabbychancer@gmail.com>
2026-08-19 16:42:48 +0200

    Fix adapter withdraw to use asset amounts + pay referral rewards from fees

--- contracts/AssetPool.sol
+++ contracts/AssetPool.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./xToken.sol";
 import "./ReferralRegistry.sol";
+import "./Treasury.sol";
 import "./adapters/YearnAdapter.sol";
 
 contract AssetPool is ReentrancyGuard, Pausable, Ownable {
@@ -25,6 +26,7 @@ contract AssetPool is ReentrancyGuard, Pausable, Ownable {
 
     event Deposited(address indexed user, uint256 amount, uint256 sharesMinted, uint256 feePaid);
     event Withdrawn(address indexed user, uint256 sharesBurned, uint256 amountReceived, uint256 feePaid);
+    event ReferralRewardPaid(address indexed user, address indexed referrer, uint256 amount);
 
     constructor(
         address _underlyingAsset,
@@ -55,6 +57,29 @@ contract AssetPool is ReentrancyGuard, Pausable, Ownable {
         return (totalAssets() * 1e18) / supply;
     }
 
+    /// @dev Splits a fee between the referrer (if any) and the treasury according to Treasury.referralBps
+    function _distributeFee(address user, uint256 fee) internal {
+        if (fee == 0) return;
+
+        address referrer = referralRegistry.referrerOf(user);
+        uint256 referralShare = 0;
+
+        if (referrer != address(0)) {
+            uint256 refBps = Treasury(treasury).referralBps();
+            referralShare = (fee * refBps) / BPS_DENOMINATOR;
+
+            if (referralShare > 0) {
+                underlyingAsset.safeTransfer(referrer, referralShare);
+                emit ReferralRewardPaid(user, referrer, referralShare);
+            }
+        }
+
+        uint256 treasuryShare = fee - referralShare;
+        if (treasuryShare > 0) {
+            underlyingAsset.safeTransfer(treasury, treasuryShare);
+        }
+    }
+
     function deposit(uint256 amount, address referrer) external nonReentrant whenNotPaused {
         require(amount > 0, "Zero amount");
 
@@ -69,9 +94,9 @@ contract AssetPool is ReentrancyGuard, Pausable, Ownable {
         uint256 supply = shareToken.totalSupply();
 
         underlyingAsset.safeTransferFrom(msg.sender, address(this), amount);
-        if (fee > 0) {
-            underlyingAsset.safeTransfer(treasury, fee);
-        }
+
+        // Distribute fee: part to referrer (if any), rest to treasury
+        _distributeFee(msg.sender, fee);
 
         uint256 sharesToMint;
         if (supply == 0 || currentAssets == 0) {
@@ -80,7 +105,7 @@ contract AssetPool is ReentrancyGuard, Pausable, Ownable {
             sharesToMint = (netAmount * supply) / currentAssets;
         }
 
-        if (address(yieldAdapter) != address(0)) {
+        if (address(yieldAdapter) != address(0) && netAmount > 0) {
             underlyingAsset.safeIncreaseAllowance(address(yieldAdapter), netAmount);
             yieldAdapter.deposit(netAmount);
         }
@@ -97,16 +122,22 @@ contract AssetPool is ReentrancyGuard, Pausable, Ownable {
         uint256 grossAmount = (shareAmount * totalAssets()) / supply;
         shareToken.burn(msg.sender, shareAmount);
 
+        // Only withdraw from the yield adapter the amount we are short of
+        // (i.e. if idle balance is insufficient)
         if (address(yieldAdapter) != address(0)) {
-            yieldAdapter.withdraw(shareAmount, address(this));
+            uint256 idle = underlyingAsset.balanceOf(address(this));
+            if (idle < grossAmount) {
+                uint256 toWithdraw = grossAmount - idle;
+                yieldAdapter.withdraw(toWithdraw, address(this));
+            }
         }
 
         uint256 fee = (grossAmount * withdrawFeeBps) / BPS_DENOMINATOR;
         uint256 netAmount = grossAmount - fee;
 
-        if (fee > 0) {
-            underlyingAsset.safeTransfer(treasury, fee);
-        }
+        // Distribute fee: part to referrer (if any), rest to treasury
+        _distributeFee(msg.sender, fee);
+
         underlyingAsset.safeTransfer(msg.sender, netAmount);
 
         emit Withdrawn(msg.sender, shareAmount, netAmount, fee);
